8d7f0c23fa3d5f93642a7b0827c6c431ebbb5657
Gabriel Baiak <gabriel100baiak@gmail.com>
2019-11-22 00:40:32 -0300

    tentando pesquisa

--- src/main/java/com/project/santak/controller/IndexController.java
+++ src/main/java/com/project/santak/controller/IndexController.java
@@ -1,13 +1,33 @@
 package com.project.santak.controller;
 
+import com.project.santak.model.Produto;
+import com.project.santak.repository.ProdutosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @Autowired
+    ProdutosRepository pig;
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
     public String index(){
         return "index";
+    }
+
+    @RequestMapping(value="/", method= RequestMethod.POST)
+    public ModelAndView listaProduto(@RequestParam String search){
+        System.out.println("teste 1");
+        ModelAndView mv = new ModelAndView("index");
+        Iterable<Produto> produtos = pig.findByName(search);
+        mv.addObject("index", produtos);
+
+        System.out.println("teste 4");
+        return mv;
     }
 }
--- src/main/java/com/project/santak/repository/ProdutosRepository.java
+++ src/main/java/com/project/santak/repository/ProdutosRepository.java
@@ -2,10 +2,15 @@ package com.project.santak.repository;
 
 import com.project.santak.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
-
+@Repository
 public interface ProdutosRepository extends CrudRepository<Produto, String> {
    Produto findById(long id);
+
+   @Query("from Produto where cor like :search")
+   Iterable<Produto> findByName(@Param("search")String search);
 }
